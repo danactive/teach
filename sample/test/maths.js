4c173675f00dc3b151b46440d50bda008281458f
danactive <githubcom@domaindesign.ca>
2015-02-18 11:56:10 -0800

    Add multiply tests

--- sample/test/maths.js
+++ sample/test/maths.js
@@ -12,7 +12,21 @@ describe('Mathematical formula', function(){
 			expect(maths.add(1,-20)).to.be.lessThan(1);
 		});
 	});
-	describe('Subtraction', function(){
-		// todo write tests
+	describe('Multiply', function(){
+		it('should pass with positive values', function(){
+			expect(maths.multiply(5,9)).to.be(45);
+			expect(maths.multiply(10,3)).to.be(30);
+		});
+		it('should pass with negative values', function(){
+			expect(maths.multiply(-5,-9)).to.be(45);
+			expect(maths.multiply(10,-3)).to.be(-30);
+		});
+		it('should fail with error', function(){
+			expect(maths.multiply()).to.be(null);
+			expect(maths.multiply(2)).to.be(null);
+		});
+		it('should fail with string', function(){
+			expect(maths.multiply({},"/")).to.be(null);
+		});
 	});
 });
